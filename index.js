ce8efe3a05bd29afba5fc484be7397bfbf34f8f2
Laurens <laurens.aarnoudse@gmail.com>
2016-12-03 01:42:01 +0100

    finished tutorial altough it has some nice ideas for bonus features at the bottom that ill add next

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.broadcast.emit('hi'); //I cant find the result of this anywhere in the app
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 

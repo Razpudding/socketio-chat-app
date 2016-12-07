d7ca67f99e8c5062f337bef3e8ba8e9315e8d65e
Laurens <laurens.aarnoudse@gmail.com>
2016-12-07 17:29:25 +0100

    Finished the tutorial with the added 'feature' of a public folder to host html and css

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-// TODO: Fix external CSS, apparently loading it with the normal local method wont work
-// As the file needs to be served by the server. This shows how to do just that:
-// http://stackoverflow.com/questions/12134554/node-js-external-js-and-css-files-just-using-node-js-not-express?lq=1
-
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.broadcast.emit('hi'); //I cant find the result of this anywhere in the app
-
+  socket.broadcast.emit('user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
@@ -26,6 +22,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3000, function(){
-	console.log("You're listening to *:3000, listener supported radio");
+  console.log("You're listening to *:3000, listener supported radio");
 });
-

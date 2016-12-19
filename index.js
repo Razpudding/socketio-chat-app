275bc9b5244dfb8d0ab73d4caada14f6a598f227
Laurens <laurens.aarnoudse@gmail.com>
2016-12-19 18:04:44 +0100

    usernames are now supported and log off messages are displayed when a user disconnects

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
 This code is adapted from the socket.io example: http://socket.io/get-started/chat/
 TODO:
-Broadcast a message to connected users when someone connects or disconnects
-Add support for nicknames
-Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+v Broadcast a message to connected users when someone connects or disconnects
+V Add support for nicknames
+V Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
 Add “{user} is typing” functionality
 Show who’s online
 Add private messaging
@@ -15,25 +15,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
-
+//When a new client connects
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.broadcast.emit('newUser', socket.id); //Change this later to emit the username also add message when someone leaves
+  socket.broadcast.emit('newUser', socket.id); 
   
-  socket.on('setUserName', function(name){
+  socket.on('setUserName', function(name){// Change to emit to all
     console.log("detected new username");
     socket.username = name;
-    socket.broadcast.emit('newName', socket.id, socket.username); 
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+    io.sockets.emit('newName', socket.id, socket.username); //Sends message to all sockets
   });
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     socket.broadcast.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    io.sockets.emit('userLeft', socket.username);
+    console.log('user disconnected', socket.username);
   });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,9 +1,7 @@
-//Ugh inline code
 var socket = io();
+var userName;
+var myId;
 window.addEventListener('keydown',this.keyDown,false);
-//$('#userNameWrapper').addEventListener('submit', )
-//userNameWrapper.preventDefault();
-
 
 $('#messageInput').submit(function(){
   var msg = $('#m').val();
@@ -14,11 +12,16 @@ $('#messageInput').submit(function(){
   return false;
 });
 
+socket.on('connect', function(){
+  console.log("connection to server made");
+  console.log(socket.io.engine.id);
+  myId = socket.io.engine.id;
+});
+
 //When receiving a chat message from another user, show it in the DOM
 socket.on('chat message', function(msg){
   console.log("new message detected");
   $('#messages').append($('<li>').text(msg));
-
 });
 //When receiving a new user event, print the user ID to the console (remove later)
 socket.on('newUser', function(id){
@@ -26,21 +29,33 @@ socket.on('newUser', function(id){
   //$('#messages').append($('<li>').text(id));
 });
 //When a new userName is received, print it to the console
-socket.on('newName', function(name, id){
+socket.on('newName', function(id, name){
   console.log("new username for user: " + id + " name: " + name);
-  //$('#messages').append($('<li>').text(msg));
+  if (id == myId)
+  {
+    console.log("a girl has a name");
+    userName = name;
+  }
+  //else {  $('#messages').append($('<li>').text(msg)); }
+});
+
+socket.on('userLeft', function(name){
+  console.log("user disconnected" + name);
+  $('#messages').append($('<li>').text(name + " has left the building"));
 });
 
 //Key listener to listen to enter event. Is now used to send the username and later to send messages as well
 function keyDown(event) {
   if (event.key == "Enter"){
-    socket.emit('setUserName', $('#usernameInput').val());
-    $('#userNameWrapper').toggle();
-    console.log("Username set: " + $('#usernameInput').val());
-  }
-  else {
-    console.log("non enter key pressed");
-  }
+    if(!userName)
+    {
+      socket.emit('setUserName', $('#usernameInput').val());
+      $('#userNameWrapper').toggle();
+      console.log("Sending username: " + $('#usernameInput').val());
+    }
+    else {
+        //console.log("non enter key pressed");
+    }
   /*
     // Auto-focus the current input when a key is typed
     if (!(event.ctrlKey || event.metaKey || event.altKey)) {
@@ -57,4 +72,6 @@ function keyDown(event) {
       }
     }
     */
- }
+
+  }
+}
